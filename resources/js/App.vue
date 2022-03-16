065ea9224eb51b0bd30a74bf50201cf5c3c6cef2
CIKADA <cikada@gmail.com>
2022-03-16 23:51:56 +0300

    install laravel debugbar, vue3, vuex

---
+++ resources/js/App.vue
@@ -0,0 +1,20 @@
+<template>
+  
+  <h1>Laravel + Vue = Blog</h1>
+  <div class="container">
+    <div class="row">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolore veritatis incidunt non, possimus atque autem ab accusantium aliquam voluptate amet eum voluptatibus libero alias ullam aperiam pariatur deleniti reiciendis debitis, a voluptatum perferendis quia at molestias. Saepe ad ratione commodi, maxime sapiente asperiores cum dicta ducimus neque nulla illo.</p>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
